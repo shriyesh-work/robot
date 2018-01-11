af12bf6483a2c95d33db1e8e221d439bb3dbdd74
shriyesh pujari <shriyesh@techverito.com>
2018-01-11 18:48:21 +0530

    Robot returns position history

--- robot.rb
+++ robot.rb
@@ -8,7 +8,6 @@ class Robot
   RIGHT = {:NORTH => :EAST, :EAST => :SOUTH, :SOUTH => :WEST, :WEST => :NORTH}
   LEFT = {:NORTH => :WEST, :WEST => :SOUTH, :SOUTH => :EAST, :EAST => :NORTH}
 
-
   def initialize(x, y, direction)
     @positions = [ {x: x, y: y} ]
     @direction = direction
@@ -17,11 +16,29 @@ class Robot
 
   def calculate_adjacent_positions
     current_position = @positions.last
-    left = {x: current_position[:x]-1, y: current_position[:y]}
-    right = {x: current_position[:x]+1, y: current_position[:y]}
-    front = {x: current_position[:x], y: current_position[:y]-1}
-    back = {x: current_position[:x], y: current_position[:y]+1}
-    @adjacent_positions = {left: left, front: front, right: right, back: back}
+    case @direction
+    when :NORTH 
+      front = {x: current_position[:x], y: current_position[:y]-1}
+      back = {x: current_position[:x], y: current_position[:y]+1}
+      left = {x: current_position[:x]-1, y: current_position[:y]}
+      right = {x: current_position[:x]+1, y: current_position[:y]} 
+    when :EAST
+      front = {x: current_position[:x]+1, y: current_position[:y]}
+      back = {x: current_position[:x]-1, y: current_position[:y]}
+      left = {x: current_position[:x], y: current_position[:y]-1}
+      right = {x: current_position[:x], y: current_position[:y]+1} 
+    when :SOUTH
+      front = {x: current_position[:x], y: current_position[:y]+1}
+      back = {x: current_position[:x], y: current_position[:y]-1}
+      left = {x: current_position[:x]+1, y: current_position[:y]}
+      right = {x: current_position[:x]-1, y: current_position[:y]} 
+    when :WEST
+      front = {x: current_position[:x]-1, y: current_position[:y]}
+      back = {x: current_position[:x]+1, y: current_position[:y]}
+      left = {x: current_position[:x], y: current_position[:y]+1}
+      right = {x: current_position[:x], y: current_position[:y]-1} 
+    end
+    @adjacent_positions = {front: front, right: right, back: back, left: left}
   end
 
   def get_position
@@ -38,10 +55,14 @@ class Robot
 
   def turn_right
     @direction = RIGHT[@direction]
+    calculate_adjacent_positions
+    @direction
   end
 
   def turn_left
     @direction = LEFT[@direction]
+    calculate_adjacent_positions
+    @direction
   end
 
   def move_forward
@@ -50,7 +71,7 @@ class Robot
     get_position
   end
 
-  def move_back
+  def move_backward
     @positions.push(@adjacent_positions[:back])
     calculate_adjacent_positions
     get_position
@@ -61,13 +82,15 @@ class Robot
     calculate_adjacent_positions
     get_position
   end
-  
+
   def move_right
     @positions.push(@adjacent_positions[:right])
     calculate_adjacent_positions
     get_position
   end
 
-
+  def get_position_history
+    @positions
+  end
 
 end
--- robot_test.rb
+++ robot_test.rb
@@ -7,7 +7,7 @@ class RobotTest < Minitest::Test
   end
 
   def test_robot_is_placed_at_given_loaction
-    expected_position = {:x => 2, :y => 4}
+    expected_position = {x: 2, y: 4}
     assert_equal expected_position, @robot.get_position
   end
 
@@ -24,29 +24,51 @@ class RobotTest < Minitest::Test
   end
 
   def test_robot_moves_forward 
-    expected_position = {:x => 2, :y => 3}
+    expected_position = {x: 2, y: 3}
     assert_equal expected_position, @robot.move_forward
   end
 
-  def test_robot_moves_back
+  def test_robot_moves_backward
     @robot.move_forward
-    expected_position = {:x => 2, :y => 4}
-    assert_equal expected_position, @robot.move_back
+    expected_position = {x: 2, y: 4}
+    assert_equal expected_position, @robot.move_backward
   end
 
   def test_robots_adjacent_positions
-    adjacent_positions = {left: { x: 1, y: 4}, front: { x: 2, y: 3}, right: { x: 3, y: 4}, back: { x: 2, y: 5}}
-    assert_equal adjacent_positions, @robot.get_adjacent_positions
+    expected_positions = {front: { x: 2, y: 3}, right: { x: 3, y: 4}, back: { x: 2, y: 5}, left: { x: 1, y: 4}}
+    assert_equal expected_positions, @robot.get_adjacent_positions
   end
 
   def test_robot_moves_left
-    expected_position = {:x => 1, :y => 4}
+    expected_position = {x: 1, y: 4}
     assert_equal expected_position, @robot.move_left
   end
 
   def test_robot_moves_right
-    expected_position = {:x => 3, :y => 4}
+    expected_position = {x: 3, y: 4}
     assert_equal expected_position, @robot.move_right
+  end
+
+  def test_adjacent_positions_when_turned_right
+    @robot.turn_right
+    expected_positions = {front: { x: 3, y: 4}, right: { x: 2, y: 5}, back: { x: 1, y: 4}, left: { x: 2, y: 3}}
+    assert_equal expected_positions, @robot.get_adjacent_positions
+  end
+
+  def test_adjacent_positions_when_turned_left
+    @robot.turn_left
+    expected_positions = {:front=>{:x=>1, :y=>4}, :right=>{:x=>2, :y=>3}, :back=>{:x=>3, :y=>4}, :left=>{:x=>2, :y=>5}}
+    assert_equal expected_positions, @robot.get_adjacent_positions
+  end
+
+  def test_robot_position_history
+    @robot.move_forward
+    @robot.turn_left
+    @robot.move_forward
+    @robot.move_backward
+    @robot.move_left
+    expected_positions_history = [{x: 2, y: 4}, {x: 2, y: 3}, {x: 1, y: 3}, {x: 2, y: 3}, {x: 2, y: 4}]
+    assert_equal expected_positions_history, @robot.get_position_history
   end
 
 end
